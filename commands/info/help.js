f232bf02fddee5eec9904dabc6edc46f2ed3ca2d
Kamil Jodłowski <102437351+SkynetV5@users.noreply.github.com>
2023-04-12 10:57:08 +0200

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -22,17 +22,19 @@ module.exports = {
             iconURL: message.author.avatarURL({dynamic: true})
         }))
         .setTitle('Lista Komend')
-        .setDescription('__Chcąc wpisać daną komendę zacznij (!) : __')
+        .setDescription('__Chcąc wpisać daną komendę zacznij (!)/If you want to write down a command begin with (!) : __')
         .addFields(
-            {name: '-help', value: '_Wyświetla wszystkie komendy_'},
-            {name: '-ban', value: '_Banuje użytkownika (!ban osoba powod). Komenda dostępna dla ról Admin oraz Moderator_'},
-            {name: '-unban', value: '_Odbanuje użytkownika (!unban osoba). Komenda dostępna dla ról Admin oraz Moderator_'},
-            {name: '-kick', value: '_Wyrzuca użytkownika (!kick osoba powod). Komenda dostępna dla ról Admin oraz Moderator_'},
-            {name: '-mute', value: '_Wycisza użytkownika (!mute osoba). Komenda dostępna dla ról Admin oraz Moderator_'},
-            {name: '-unmute', value: '_Odcisza użytkownika (!unmute osoba). Komenda dostępna dla ról Admin oraz Moderator_'},
-            {name: '-avatar', value :'_Wyświetla avatar użytkownika (bez oznaczenia użytkownika - wyświetli avatar osoby wpisującej komende)_'},
-            {name: '-kiss', value :'_Całuje danego użytkownika (!kiss osoba)_'},
-            {name: '-poke', value :'_Zaczepia gracza (!poke osoba)_'},
+            { name: 'Dla wszystkich/For everyone:', value: '\u200B' },
+            {name: '-help', value: '_Wyświetla wszystkie komendy/Shows all commands_'},
+            {name: '-avatar', value :'_Wyświetla avatar użytkownika (bez oznaczenia użytkownika - wyświetli avatar osoby wpisującej komende)/Shows users avatar(without pointed user yours avatar will be displayed)_'},
+            {name: '-kiss', value :'_Całuje danego użytkownika (!kiss osoba)/Kisses pointed user_'},
+            {name: '-poke', value :'_Zaczepia gracza (!poke osoba)/Pokes pointed user_'},
+            { name: 'Dostępne dla Admina oraz Moderatora!/Restricted for Admin and Moderator only!', value: '\u200B' },
+            {name: '-ban', value: '_Banuje użytkownika (!ban osoba powod)._'},
+            {name: '-unban', value: '_Odbanuje użytkownika (!unban osoba)._'},
+            {name: '-kick', value: '_Wyrzuca użytkownika (!kick osoba powod)._'},
+            {name: '-mute', value: '_Wycisza użytkownika (!mute osoba)._'},
+            {name: '-unmute', value: '_Odcisza użytkownika (!unmute osoba)._'}
         )
         .setTimestamp()
         .setFooter({
